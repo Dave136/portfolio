a99313258f85fcc9d3a171e0b5f8da00b56d6c0f
Dave136 <davejs136@gmail.com>
2022-08-03 09:27:02 -0400

    fix: cards position

--- src/lib/components/card-wrapper.svelte
+++ src/lib/components/card-wrapper.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-col bg-primary dark:bg-primary-alt m-auto p-auto mt-12 sm:mt-16 z-0 lg:mt-8">
   <div class="flex overflow-x-scroll pb-10 hide-scroll-bar snap-proximity">
-    <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10">
+    <div class="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 first:ml-0">
       <slot />
     </div>
   </div>
