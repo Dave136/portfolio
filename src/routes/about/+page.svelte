67d11c7017764a5cf6ee0f347ec2fb74a1ba1a30
David Arenas <davejs136@gmail.com>
2023-05-23 18:56:16 -0400

    refactor: about section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -33,17 +33,18 @@
   <meta name="description" content="About me" />
 </svelte:head>
 
-<section class="px-8 flex flex-col items-center">
+<section class="px-2 flex flex-col items-center">
+  <h2 class="text-3xl self-start mt-8 mb-4 font-bold">{$t('about.title')}</h2>
   <picture class="mt-2 sm:mt-10 lg:mt-2">
     <source srcset="me2.jpg" media="(min-width: 800px)" />
-    <img class="rounded-full w-48 h-48 sm:w-64 sm:h-64 object-cover" src="me2.jpg" alt="Me" />
+    <img class="rounded-md w-full h-48 sm:w-64 sm:h-64 object-cover" src="me2.jpg" alt="Me" />
   </picture>
-  <p class="text-sm mt-4 px-6 sm:w-11/12 sm:text-base sm:mt-8 lg:mt-4 lg:w-9/12">
+  <p class="w-full text-base mt-8 px-2 sm:mt-8 lg:mt-4 lg:w-9/12">
     <span class="text-xl">{$t('about.greating')}</span>
     {$t('about.description')}
   </p>
   <div
-    class="w-[85%] min-h-full grid grid-cols-3 gap-4 mt-8 sm:grid-cols-4 sm:mt-14 lg:mt-8 lg:grid-cols-6 lg:w-[70%]"
+    class="w-full min-h-full grid grid-cols-3 gap-4 my-8 sm:grid-cols-4 sm:mt-14 lg:mt-8 lg:grid-cols-6 lg:w-[70%]"
   >
     {#each logos as logo}
       <div class="flex justify-center">
